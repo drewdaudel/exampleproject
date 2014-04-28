41ed00e8cb0ac21a5a72f2971704933a08b3eaac
drewdaudel <drewdaudel@msn.com>
2014-04-28 10:45:58 -0700

    First commit

---
+++ src/examplepackage/Drew.java
@@ -0,0 +1,17 @@
+package examplepackage;
+
+
+
+import javax.swing.*;
+
+public class Drew {
+	public static void main (String[] args) {
+		TheWindow w = new TheWindow();
+		
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		w.setSize(230,280);
+		w.setVisible(true);
+		
+	}
+}
+
